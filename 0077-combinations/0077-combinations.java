5b3a5b24c128ea377f159aa84587543fb8d4e58d
AyushB21 <117504082+AyushB21@users.noreply.github.com>
2023-07-29 22:57:30 +0530

    Time: 18 ms (73.06%), Space: 45.3 MB (76.38%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,20 +1,24 @@
 class Solution {
-  public List<List<Integer>> combine(int n, int k) {
-    List<List<Integer>> ans = new ArrayList<>();
-    dfs(n, k, 1, new ArrayList<>(), ans);
-    return ans;
-  }
-
-  private void dfs(int n, int k, int s, List<Integer> path, List<List<Integer>> ans) {
-    if (path.size() == k) {
-      ans.add(new ArrayList<>(path));
-      return;
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> l=new ArrayList<>();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=i+1;
+        }
+        bt(0,res,l,k,arr);
+        return res;
     }
-
-    for (int i = s; i <= n; ++i) {
-      path.add(i);
-      dfs(n, k, i + 1, path, ans);
-      path.remove(path.size() - 1);
+    void bt(int i,List<List<Integer>> res,List<Integer> l,int k,int arr[]){
+        if(i==arr.length){
+            if(l.size()==k){
+                res.add(new ArrayList<>(l));
+            }
+            return;
+        }
+        l.add(arr[i]);
+        bt(i+1,res,l,k,arr);
+        l.remove(l.size()-1);
+        bt(i+1,res,l,k,arr);
     }
-  }
 }
